ff084213f2d365dbfaa391ad773665abe8f08909
Md Shadan Altmash <31220568+mdshadanaltmash@users.noreply.github.com>
2017-10-31 09:33:59 +0530

    Add files via upload

---
+++ eulertest5.py
@@ -0,0 +1,12 @@
+#smallest +ve int divisible by all the number from 1 to 20
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+a=1
+while a>0:
+    if a%20==0 and a%19==0 and a%18==0 and a%17==0 and a%16==0 and a%15==0 and a%14==0 and a%13==0 and a%12==0 and a%11==0:
+        print (a)
+        break
+    a+=1
